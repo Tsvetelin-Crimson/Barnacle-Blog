e436ea8348de0bcbba1c7596a496131cd5736c61
Tsvetelin-Crimson <67798636+Tsvetelin-Crimson@users.noreply.github.com>
2022-11-12 19:43:49 +0200

    Added html for login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  username?: string;
+  //fullForm = new FormGroup('');
+  password?: string;
+  constructor() {
 
-  constructor() { }
+   }
+
+  login($event: MouseEvent): void {
+    $event.preventDefault();
+    console.log(this.username, this.password);
+    
+  }
 
   ngOnInit(): void {
   }
